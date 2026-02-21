d99245864b1db5fc06d5a62880086443b894ef55
Krish <3dstudent1997@gmail.com>
2026-02-21 12:32:21 +0530

    renamed

--- mongodb.sh
+++ mongodb.sh
@@ -31,15 +31,15 @@ VALIDATE "$?" "copying mongo.repo"
 dnf install mongodb-org -y   &>>$LOGS_FILE
 VALIDATE "$?" "Mongodb installed"
 
-systemctl enable mongod 
+systemctl enable mongod &>>$LOGS_FILE
 VALIDATE "$?" "systemctl enable mongodb"
 
 
-systemctl start mongod 
+systemctl start mongod &>>$LOGS_FILE
 VALIDATE "$?" "systemctl start mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE "$? "Allowing remote connections"
 
-systemctl restart mongod
-VALIDATE "$?" "systemctl restart mongodb"   
+systemctl restart mongod &>>$LOGS_FILE
+VALIDATE $? "systemctl restart mongodb"
