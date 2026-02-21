c50dcc5cadc3c829318810eb02f8e1f49b4588c5
Krish <3dstudent1997@gmail.com>
2026-02-21 23:03:59 +0530

    updating

--- redis.sh
+++ redis.sh
@@ -24,3 +24,21 @@ VALIDATE(){
         echo -e "$2 ..... is $G Success $N" | tee $LOGS_FILE
     fi
 }
+
+dnf module disable redis -y &>>$LOGS_FILE
+VALIDATE $? "Disable redis"
+
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable redis"
+
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $?  "Install redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 'protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+
+systemctl enable redis 
+systemctl start redis 
+VALIDATE $?  "Enable and Start redis"
