89b0b11363365084271070c8dfc9ac6481ef8860
Krish <3dstudent1997@gmail.com>
2026-02-22 13:27:50 +0530

    cart

--- frontend.sh
+++ frontend.sh
@@ -28,3 +28,41 @@ VALIDATE(){
         echo -e "$2 ...is $G Success $N" | tee $LOGS_FILE
     fi
 }
+
+dnf module disable nginx -y
+VALIDATE $? "Disable Nginx"
+
+dnf module enable nginx:1.24 -y
+VALIDATE $? "Enable Nginx"
+
+dnf install nginx -y
+VALIDATE $? "Installed Nginx"
+
+systemctl enable nginx 
+systemctl start nginx 
+VALIDATE $? "start and enabled Nginx"
+
+
+mkdir -p /app 
+VALIDATE $? "app created"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Removing code"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip 
+VALIDATE $? "frontend download"
+
+cd /usr/share/nginx/html  
+VALIDATE $? "Moving to HTML directory"
+
+unzip /tmp/frontend.zip
+VALIDATE $? "Unzip code"
+
+rm -rf /etc/nginx/nginx.conf
+VALIDATE $? "removing old conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied our nginx conf file"
+
+systemctl restart nginx 
+VALIDATE $? "Restarted Nginx"
