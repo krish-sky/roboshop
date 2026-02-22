aa32b7490a14b90b74b4973d5ac7b89b3e4b5808
Krish <3dstudent1997@gmail.com>
2026-02-22 11:54:36 +0530

    cart

---
+++ cart.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPTDIR=$PWD
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...is $R failure $N" | tee $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ...is $G Success $N" | tee $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disable Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "enable Nodejs"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "install Nodejs"
+
+id roboshop
+
+if [ $? -ne 0 ]; then   
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "System User created"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "app created"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip 
+VALIDATE $? "cart download"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "removing existing code"
+
+unzip /tmp/cart.zip
+VALIDATE $? "Unzip code"
+
+
+npm install  &>>$LOGS_FILE
+VALIDATE $? "npm install"
+
+cp $SCRIPTDIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copying cart.service file"
+
+systemctl daemon-reload
+systemctl enable cart 
+systemctl start cart
+VALIDATE $? "reload enable and start cart"
+
+cp $SCRIPTDIR/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "mongodb-org install"
+
+
